80cbcf3386b4337c6d42aa79f4520a2e1ba30162
uae <aniekaudo@yahoo.com>
2022-11-08 20:27:41 +0100

    :busts_in_silhouette: Testimonials
    Add testimonials

--- src/components/Testimonial/fakeData.js
+++ src/components/Testimonial/fakeData.js
@@ -1,6 +1,6 @@
 import image1 from '../../assets/home/testimonial1.png';
-import image2 from '../../assets/home/testimonial2.png';
-import image3 from '../../assets/home/testimonial2.png';
+import image2 from '../../assets/home/test.png';
+import image3 from '../../assets/home/test1.png';
 
 export const fakeData = [
     {
@@ -15,7 +15,7 @@ export const fakeData = [
         img: image2,
         name: 'Miles Esther',
         profession: 'Public Relations Specialist',
-        message: "You won't regret it. I was amazed at the quality of it. I am really satisfied with my it."
+        message: "You won't regret it. I was amazed at the quality of your products. I am really satisfied with my it."
     },
     {
         id: 3,
@@ -29,20 +29,20 @@ export const fakeData = [
         img: image1,
         name: 'Howard Arlene',
         profession: 'Finance Manager',
-        message: "Your company is truly upstanding and is behind its product 100%. It's the perfect solution for our business. It has really helped our business."
+        message: "The quality of your product is second to none, my team are really satisfied."
     },
     {
         id: 5,
         img: image2,
         name: 'Miles Esther',
         profession: 'Public Relations Specialist',
-        message: "You won't regret it. I was amazed at the quality of it. I am really satisfied with my it."
+        message: "It truly meet my needs, the fast delivery before the agreed time and the quality of the product is amazing. I look forward to doing business with you again"
     },
     {
         id: 6,
         img: image3,
         name: 'Pena Norma',
         profession: 'Public Relations Specialist',
-        message: "It fits our needs perfectly. Man, this thing is getting better and better as I learn more about it. You've saved our business!"
+        message: "You really saved my business with the amazing website you created, my bussiness have garnered more sales since I got global, I look forward to more years of doing business together"
     },
 ]
