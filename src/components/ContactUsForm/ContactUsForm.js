602b1dead95ed83d7dbe7599f55d99915ecb4b91
uae <aniekaudo@yahoo.com>
2022-11-08 09:51:08 +0100

    :art: Email structure
    Implement email in form contact

--- src/components/ContactUsForm/ContactUsForm.js
+++ src/components/ContactUsForm/ContactUsForm.js
@@ -1,70 +1,142 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from 'react';
 import styles from './ContactUsForm.module.css';
+import * as emailjs from 'emailjs-com';
 // import {Map, GoogleApiWrapper} from 'google-maps-react';
 
-const ContactUsForm = () =>{
-    const [formContent, setFormContent ] = useState({});
+const ContactUsForm = () => {
+  const form = useRef();
+  const [done, setDone] = useState(false);
 
-    const handleChange = ( event ) =>{
-        const { value, name } = event.target;
+  const [formContent, setFormContent] = useState({});
 
-        setFormContent(prevState => ({
-            ...prevState,
-            [name]: value
-        }))
-    }
+  const handleChange = (event) => {
+    const { value, name } = event.target;
 
+    setFormContent((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+  };
 
-    return(
-        <>
-            <div className={`container ${styles.contain} overflow-hidden`}>
-                <p data-aos='fade-up' className={`${styles.heading} mx-auto`}>We&apos;d Love to Hear From You</p>
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-                <div className="row">
-                    <div className="col-md-4" data-aos='fade-right'>
-                        <p className={styles.head}>Address</p>
-                        <p className={styles.content}>1201 New Zealand Metropolis 3452</p>
-                    </div>
+    emailjs
+      .sendForm(
+        'service_cl12pdl',
+        'template_77v390w',
+        form.current,
+        'pgmVPEfjs8MRwWdxp'
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+          form.reset();
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
 
-                    <div className="col-md-4" data-aos='fade-up' data-aos-offset='100'>
-                        <p className={styles.head}>Contact</p>
-                        <p className={styles.content}>Mobile 	: +00 123 456 78</p> 
-                        <p className={styles.content}>Phone 	: +00 123 678 90</p>
-                        <p className={styles.content}>Email: contact@gmail.com</p>
-                    </div>
+  return (
+    <>
+      <div className={`container ${styles.contain} overflow-hidden`}>
+        <p data-aos="fade-up" className={`${styles.heading} mx-auto`}>
+          We&apos;d Love to Hear From You
+        </p>
 
-                    <div className="col-md-4" data-aos='fade-left' data-aos-offset='70'>
-                        <p className={styles.head}>Address</p>
-                        <p className={styles.content}>Monday - Friday: 09:00 - 20:00</p>
-                        <p className={styles.content}>Sunday &amp; Saturday: 10:30 - 22:00</p>
-                    </div>
-                </div>
+        <div className="row">
+          <div className="col-md-4" data-aos="fade-right">
+            <p className={styles.head}>Address</p>
+            <p className={styles.content}>1201 New Zealand Metropolis 3452</p>
+          </div>
 
-                <p className={styles.email} data-aos='fade-up'>You can email us</p>
+          <div className="col-md-4" data-aos="fade-up" data-aos-offset="100">
+            <p className={styles.head}>Contact</p>
+            <p className={styles.content}>Mobile : +00 123 456 78</p>
+            <p className={styles.content}>Phone : +00 123 678 90</p>
+            <p className={styles.content}>Email: contact@gmail.com</p>
+          </div>
 
-                <div className="row">
-                    <div className="col-md-6" data-aos='fade-right' style={{textAlign: 'left'}}>
-                        <label className={styles.label}>Name</label><br/>
-                        <input name="name" value={formContent.name} onChange={handleChange} className={styles.input} type="text" />
+          <div className="col-md-4" data-aos="fade-left" data-aos-offset="70">
+            <p className={styles.head}>Address</p>
+            <p className={styles.content}>Monday - Friday: 09:00 - 20:00</p>
+            <p className={styles.content}>
+              Sunday &amp; Saturday: 10:30 - 22:00
+            </p>
+          </div>
+        </div>
 
-                        <label className={styles.label}>Email</label><br/>
-                        <input name="email" value={formContent.email} onChange={handleChange} className={styles.input} type="email" />
+        <p className={styles.email} data-aos="fade-up">
+          You can email us
+        </p>
 
-                        <label className={styles.label}>Subject</label><br/>
-                        <input name="subject" value={formContent.subject} onChange={handleChange} className={styles.input} type="text" />
-                    </div>
+          <form ref={form} onSubmit={sendEmail}>
+        <div className="row">
+          <div
+            className="col-md-6"
+            data-aos="fade-right"
+            style={{ textAlign: 'left' }}
+          >
+            <label className={styles.label}>Name</label>
+            <br />
+            <input
+              name="name"
+              value={formContent.name}
+              onChange={handleChange}
+              className={styles.input}
+              type="text"
+            />
 
-                    <div className="col-md-6" data-aos='fade-left' style={{textAlign: 'left'}}>
-                        <label className={styles.label}>Message</label><br/>
-                        <textarea name="message" value={formContent.message} onChange={handleChange} className={`${styles.input} ${styles.msg}`} type="text"/>
-                        <div style={{textAlign:'right'}}>
-                            <button className={`btn custom_btn ${styles.btn}`}>SEND</button>
-                        </div>
-                    </div>
-                </div>
+            <label className={styles.label}>Email</label>
+            <br />
+            <input
+              name="email"
+              value={formContent.email}
+              onChange={handleChange}
+              className={styles.input}
+              type="email"
+            />
+
+            <label className={styles.label}>Subject</label>
+            <br />
+            <input
+              name="subject"
+              value={formContent.subject}
+              onChange={handleChange}
+              className={styles.input}
+              type="text"
+            />
+          </div>
+
+          <div
+            className="col-md-6"
+            data-aos="fade-left"
+            style={{ textAlign: 'left' }}
+          >
+            <label className={styles.label}>Message</label>
+            <br />
+            <textarea
+              name="message"
+              value={formContent.message}
+              onChange={handleChange}
+              className={`${styles.input} ${styles.msg}`}
+              type="text"
+            />
+            <div style={{ textAlign: 'right' }}>
+              <button type="submit" className={`btn custom_btn ${styles.btn}`}>
+                SEND
+              </button>
             </div>
+            <span style={{color: 'green', fontFamily: 'Gumela'}}>{done && 'Thanks for Contacting me'}</span>
+          </div>
+        </div>
+          </form>
+      </div>
 
-            {/* <div>
+      {/* <div>
                 <Map
                     google={this.props.google}
                     style={{width:'100%', height:'50%'}}
@@ -77,8 +149,8 @@ const ContactUsForm = () =>{
                     }
                 />
             </div> */}
-        </>
-    )
-}
+    </>
+  );
+};
 
 export default ContactUsForm;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,54 +5,68 @@ import linkdin from '../../assets/home/linkdin.png';
 import insta from '../../assets/home/insta.png';
 
 const Footer = () =>{
+    const Foter = () => new Date().getFullYear();
     return (
-        <>
-            <div style={{backgroundColor:'#2b2b2b', paddingTop:'89px', paddingBottom:'90px'}}>
-                <div className="container" >
-                    <div className="row">
-                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
-                            <p className={styles.logo}>Prosoft</p>
-                            <p className={styles.text}>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed</p>
-                            <p className={styles.text}>Lorem ipsum dolor sit amet</p>
-                            <p className={styles.text}>Lorem ipsum dolor sit amet</p>
-                            <div className={styles.social_logo}>
-                                <img src={fb} alt="fb" className={styles.img}/>
-                                <img src={linkdin} alt="in" className={styles.img}/>
-                                <img src={insta} alt="insta" className={styles.img}/>
-                            </div>
-                        </div>
-
-                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
-                            <p className={styles.head}>Services</p>
-                            <p className={styles.text}>4517 Washington Ave. Manchester, Kentucky 39495</p>
-                            <p className={styles.text}>deanna.curtis@example.com</p>
-                            <p className={styles.text}>(671) 555-0110</p>
-
-                        </div>
-
-                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
-                            <p className={styles.head}>Head Office</p>
-                            <p className={styles.text}>4517 Washington Ave. Manchester, Kentucky 39495</p>
-                            <p className={styles.text}>deanna.curtis@example.com</p>
-                            <p className={styles.text}>(671) 555-0110</p>
-                        </div>
-
-                        <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
-                            <p className={styles.head}>Opening Hour</p>
-                            <p className={styles.text}>Mon - Sat : 8:00 - 17:00</p>
-                            <p className={styles.text}>Sun : Closed</p>
-
-                        </div>
-                    </div>
-
+      <>
+        <div
+          style={{
+            backgroundColor: '#2b2b2b',
+            paddingTop: '89px',
+            paddingBottom: '90px',
+          }}
+        >
+          <div className="container">
+            <div className="row">
+              <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
+                <p className={styles.logo}>Prosoft</p>
+                <p className={styles.text}>
+                  Stop wasting time and money designing and managing strategy
+                  that doesn’t get results. Happiness guaranteed!
+                </p>
+                <p className={styles.text}>Quick service delivery!</p>
+                <p className={styles.text}>Affordable prices!</p>
+                <div className={styles.social_logo}>
+                  <img src={fb} alt="fb" className={styles.img} />
+                  <img src={linkdin} alt="in" className={styles.img} />
+                  <img src={insta} alt="insta" className={styles.img} />
                 </div>
-            </div>
+              </div>
 
+              <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
+                <p className={styles.head}>Services</p>
+                <p className={styles.text}>
+                  Km 7 Abak road, Uyo, Akwa Ibom state
+                </p>
+                <p className={styles.text}>prosoftlimited@gmail.com</p>
+                <p className={styles.text}>(234) 80-32668874</p>
+              </div>
 
-            <div style={{backgroundColor: '#2b2b2b', borderTop: 'solid 1px #707070'}}>
-                    <p className={styles.bottom}>Copyright © 2020. All right reserved</p>
+              <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
+                <p className={styles.head}>Head Office</p>
+                <p className={styles.text}>
+                  Mojsija Zecevica, Berane, Montenegro
+                </p>
+                <p className={styles.text}>prosoftlimited@gmail.com</p>
+                <p className={styles.text}>(671) 555-0110</p>
+              </div>
+
+              <div className={`col-lg-3 col-sm-6 ${styles.contain}`}>
+                <p className={styles.head}>Opening Hours</p>
+                <p className={styles.text}>Mon - Sat : 8:00 - 17:00</p>
+                <p className={styles.text}>Sun : Closed</p>
+              </div>
             </div>
-        </>
-    )
+          </div>
+        </div>
+
+        <div
+          style={{ backgroundColor: '#2b2b2b', borderTop: 'solid 1px #707070' }}
+        >
+          <p className={styles.bottom}>
+            Copyright © {Foter()} All right reserved
+          </p>
+        </div>
+      </>
+    );
 }
 export default Footer;
